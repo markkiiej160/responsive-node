ba4a6a965143372f5e9b557406923bdad6026064
Mark <mark@yorcom.nl>
2014-12-12 14:15:15 +0100

    setup of node

---
+++ index.js
@@ -0,0 +1,23 @@
+//Requirements
+var express 		= require('express');
+var lessMiddleware 	= require('less-middleware');
+var jade 			= require('jade');
+var app 			= express();
+
+
+//Set engine jade and view folder
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'jade');
+
+//Let less know where the files are
+app.use(lessMiddleware(__dirname + '/public/style'));
+app.use(express.static(__dirname + '/public/style'));
+
+//Server listen to specified port
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
